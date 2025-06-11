6d98a9cc2845a6723d47c0efc2baed77b8bba210
SoteresX <soteresxanthopoulos@gmail.com>
2025-06-11 17:58:30 +0300

    #5

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Header.css"
+
+function Header(){
+    return <div className="headerBox">
+        <h1>Recipe Tracker</h1>
+    </div>
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import RecipeBox from '../components/RecipeBox/RecipeBox'
 import '../styles/App.css';
+import Header from "../components/Header";
 
 function Home(){
     return <>
+        <Header/>
         <RecipeBox food="Spaghetti" url="/spaghetti" rank="Not Yet Learned"/>
     </>
 }
